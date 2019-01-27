fc30e528866451abf9ef8aba58cba5973b250fac
Ibanga Enoobong Ime <ibangaenoobong@yahoo.com>
2019-01-27 19:25:17 +0100

    add test for GET /todos

--- todo-api/server/tests/server.test.js
+++ todo-api/server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First todo'
+}, {
+    text: 'Second todo'
+}];
+
 beforeEach(done => {
-    Todo.deleteMany({}).then(() => done());
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -15,7 +23,7 @@ describe('POST /todos', () => {
         request(app)
             .post('/todos')
             .send({text})
-            .expect(200)
+            .expect(201)
             .expect(res => {
                 expect(res.body.text).toBeA('string').toBe(text)
             })
@@ -23,7 +31,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err)
                 }
-                Todo.find().then(todos => {
+                Todo.find({text}).then(todos => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -36,14 +44,27 @@ describe('POST /todos', () => {
             .post('/todos')
             .send({})
             .expect(400)
-            .end((err) => {
+            .end(err => {
                 if (err) {
                     return done(err)
                 }
                 Todo.find().then(todos => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch(reason => done(reason));
             })
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done);
+
     });
 });
