e8dbf2253a4bce61673e79c638791096d180e0cf
Ibanga Enoobong Ime <ibangaenoobong@yahoo.com>
2019-01-30 17:30:24 +0100

    add geolocation support via geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,5 +11,20 @@ describe('generateMessage', () => {
         expect(message.from).toBeA('string').toBe(from);
         expect(message.text).toBeA('string').toBe(text);
         expect(message.createdAt).toBeA(Date);
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location message', () => {
+        const from = 'Enoobong';
+        const latitude = 1;
+        const longitude = 0;
+        const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+        const message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA(Date);
+        expect(message).toInclude({from, url})
+
     });
 });
