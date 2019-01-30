25230690bae5363a33c96b5b11b658e9593ed6b2
Ibanga Enoobong Ime <ibangaenoobong@yahoo.com>
2019-01-30 20:48:38 +0100

    add Mustache.js for message templates

--- chat-app/public/js/index.js
+++ chat-app/public/js/index.js
@@ -12,22 +12,36 @@ socket.on('newMessage', function (message) {
     console.log('Receiving new message', message);
 
     const formattedTime = moment(message.createdAt).format('hh:mm a');
-    const li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li)
+    const template = jQuery('#message-template').html();
+    const html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-
-    const li = jQuery('<li></li>');
-    const a = jQuery(`<a target="_blank" href="${message.url}">My Current Location!</a>`);
-
     const formattedTime = moment(message.createdAt).format('hh:mm a');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
+    const template = jQuery('#location-message-template').html();
+    const html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.append(a);
-    jQuery('#messages').append(li)
+    jQuery('#messages').append(html);
+
+
+    // const li = jQuery('<li></li>');
+    // const a = jQuery(`<a target="_blank" href="${message.url}">My Current Location!</a>`);
+    //
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href', message.url);
+    //
+    // li.append(a);
+    // jQuery('#messages').append(li)
 });
 
 jQuery('#message-form').on('submit', function (event) {
